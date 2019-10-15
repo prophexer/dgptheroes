cb01176382fb6dceddde500579a6c85b899373c3
Xavier De Pauw <xavier@depauw.ca>
2019-10-15 21:01:14 +0800

    more case changes in Controller

--- app/Controllers/hockey/Home.php
+++ app/Controllers/hockey/Home.php
@@ -7,7 +7,7 @@ class Home extends BaseController
     public function index()
     {
         //Retrieve all player data from Players Model
-        $players = new \App\Models\Hockey\Players();
+        $players = new \App\Models\hockey\Players();
         $records = $players->findAll();
         
         // get a template parser
@@ -22,7 +22,7 @@ class Home extends BaseController
     public function showme($id)
     {
         //Retrieve one player data from Players Model with $id
-        $players = new \App\Models\Hockey\Players();
+        $players = new \App\Models\hockey\Players();
         $record = $players->find($id);
         
         // get a template parser
