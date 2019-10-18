e66503eb1ed3f0698a7dc5e32cb9eb98a5f3b8a5
Mingxi <3311386889@qq.com>
2019-10-18 18:47:41 +0800

    add view(two php files)

---
+++ app/Views/crossfire/cfplayerList.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Top Crossfire Players</title>
+	</head>
+	<body>
+		<h1>Top Crossfire Players</h1>
+		{records}
+		<p><a href="/crossfire/Home/showme/{id}">{name}</a></p>
+		{/records}
+		<p><a href="/home">Home</a></p>
+	</body>
+</html>
---
+++ app/Views/crossfire/oneCfplayer.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+	<head>
+		<title>{name}</title>
+	</head>
+	<body>
+            <h2 style="color: blueviolet">{name}</h2>
+                <p><img src="/images/crossfire/{image}"/></p>
+                <p style="color:violet">Name:{name}</p>
+                <p style="color:indigo">Club:{club}</p>
+                <p style="color:blue">City:{city}</p>
+                <p style="color:green">Main weapon:{main weapon}</p>
+                <p style="color:yellowgreen">Position:{position}</p>
+                <p style="color:orange">Nickname:{nickname}</p>
+                <p><a href="/crossfire">Back</a></p>
+		<p><a href="/home">Home</a></p>
+	</body>
+</html>
+
