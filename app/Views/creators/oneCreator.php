78faedbb7d9268b05930463f3dd50aa742fa8269
tyrantheart <1226950749@qq.com>
2019-10-20 14:04:39 +0800

    fix

--- app/Views/creators/oneCreator.php
+++ app/Views/creators/oneCreator.php
@@ -6,8 +6,13 @@
 	<body>
 		<h1>{Name}</h1>
                 <p><img src="/images/creators/{image}"/></p>
-                <p>{Name},{Date of birth},{Grduate},{Country}
-                    {Game compan}.</p>
+                <p>Name:{Name}</p>
+                <p>Date of birth:{Date_of_birth}</p>
+                <p>Grduate:{Grduate}</p>
+                <p>Country:{Country}</p>
+                <p>Game_company:{Game_company}</p>
+                <p>Representative works:{Representative_works}</p>
+                <p>Identifier:{Identifier}</p>
 		<p><a href="/home">Back</a></p>
 	</body>
 </html>
