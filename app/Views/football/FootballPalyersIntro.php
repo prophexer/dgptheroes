cfa49fb2a0e112fc75b92e948995323f744d6bb7
Ahern <1358945354@qq.com>
2019-10-20 18:17:58 +0800

    Add view

---
+++ app/Views/football/FootballPalyersIntro.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+	<head>
+		<title>{name}</title>
+	</head>
+	<body>
+            <h2>{name}</h2>
+                <p><img src="/images/football/{image}"/></p>
+                <p>Name:{name}</p>
+                <p>Age:{age}</p>
+                <p>Country:{country}</p>
+                <p>Club:{club}</p>
+                <p>Place:{place}</p>
+                <p>Jersey number:{jersey number}</p>
+                <p><a href="/football">Back</a></p>
+		<p><a href="/home">Home</a></p>
+	</body>
+</html>
