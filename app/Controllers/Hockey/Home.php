a290b84d84c58cceaf5e58f298af4efdd390f146
Xavier De Pauw <xavier@depauw.ca>
2019-11-05 18:42:39 +0800

    Added the Hockey Controller as a Resource Presenter

--- app/Controllers/Hockey/Home.php
+++ app/Controllers/Hockey/Home.php
@@ -1,25 +1,49 @@
 <?php
-namespace App\Controllers\Hockey;
-use App\Controllers\BaseController;
 
-class Home extends BaseController
+namespace App\Controllers\Hockey;
+
+use CodeIgniter\RESTful\ResourcePresenter;
+
+class Home extends ResourcePresenter
 {
+    protected $modelName = 'App\Models\Hockey\Players.php';
+    
     public function index()
     {
         //Retrieve all player data from Players Model
         $players = new \App\Models\hockey\Players();
         $records = $players->findAll();
         
-        // get a template parser
-        $parser = \Config\Services::parser();
         
-        // tell it about the substitions
-        return $parser->setData(['records' => $records])
-            // and have it render the template with those
-            ->render('hockey/playerList');
+        $table = new \CodeIgniter\View\Table();
+    
+        $headings = $players->fields;
+        
+        //Add an extra row to header for column show
+        $headings[count($headings)+1] = "Show";
+            
+        $table->setHeading($headings);
+
+        foreach ($records as $val) {
+
+            //create a new array without the 'show' heading
+            $headingsminusshow = array_slice($headings, 0, count($headings)-1);
+            //Loop through headers and assign their value to an array for each table row
+            foreach ($headingsminusshow as $key => $header) {
+                $newRow[$key] = $val->$header;
+            }
+
+            //Link to show each player
+            $showLink = anchor("hockey/home/show/$val->id","Show");
+            $newRow[count($headings)+1] = $showLink;
+            
+            $table->addRow($newRow);
+        }
+        
+        return $table->generate();
     }
     
-    public function showme($id)
+    public function show($id = NULL)
     {
         //Retrieve one player data from Players Model with $id
         $players = new \App\Models\hockey\Players();
@@ -32,5 +56,44 @@ class Home extends BaseController
         return $parser->setData($record)
             // and have it render the template with those
             ->render('hockey/onePlayer');
+    }
+    
+    public function edit($id = NULL)
+    {
+        //Retrieve the player to edit
+        $players = new \App\Models\hockey\Players();
+        $record = $players->find($id);
+        
+        //set up codeigniter forms
+        helper('form');
+        
+        $headings = $players->fields;
+        
+        $hockeyForm = form_open('#');
+        
+        //Getting the headings minus id and image
+        $headingsToShow = array_slice($headings, 1, count($headings)-2);
+        
+        //Loop through the fields that can be editted using an input form
+        foreach ($headingsToShow as $key => $header) {
+            $hockeyForm .= form_label($header. ": ");
+            $hockeyForm .= form_input('$header',$record[$header]);
+            $hockeyForm .= "</br>";
+        }
+        
+        //Get all the player records to get all the image names
+        $records = $players->findAll();
+        foreach ($records as $key => $val) {
+            $images[$key] = $val->image;
+        }
+        
+        $hockeyForm .= form_dropdown($headings[count($headings)-1],$images,$id);
+        
+        $hockeyForm .= "</br>";
+        $hockeyForm .= form_button($id,"Submit");
+        
+        $hockeyForm .= form_close();
+        
+        return $hockeyForm;
     }
 }
